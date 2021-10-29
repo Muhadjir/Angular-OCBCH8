fc2f0acf7252735fb7335e71820c6732a716f3d9
Muhadjir <mmtkj0206@gmail.com>
2021-10-29 08:09:39 +0700

    Reactive Form TodoList

---
+++ 018_Muchamad Muhadjir_Assignment5/country018/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CountryService } from './country.service';
+import { HomeComponent } from './components/home/home.component';
+import { AllCountriesComponent } from './components/all-countries/all-countries.component';
+import { CountryDetailComponent } from './components/country-detail/country-detail.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AllCountriesComponent,
+    CountryDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [CountryService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 018_Muchamad Muhadjir_Assignment5/country018/src/app/components/country-detail/country-detail.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Country } from '../../country.model';
+import { CountryService } from '../../country.service';
+
+@Component({
+    selector: 'app-country-detail',
+    templateUrl: './country-detail.component.html',
+    styleUrls: ['./country-detail.component.css']
+})
+export class CountryDetailComponent implements OnInit {
+    country: Country[]=[];
+    // x: any = ' '
+
+    constructor(
+        private route: ActivatedRoute,
+        private countryService: CountryService,
+        private location: Location
+    ) { }
+
+    ngOnInit(): void {
+        this.getCountry();
+    }
+
+    getCountry(): void {
+        // this.x = this.route.snapshot.paramMap.get('name');
+        const name:any = this.route.snapshot.paramMap.get('name');
+        this.country = this.countryService.getCountry(name);
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
+}
